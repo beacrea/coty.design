529455556e2572c45d1b02e63cd54741ac753e30
Coty Beasley <beacrea@users.noreply.github.com>
2014-01-12 21:00:33 -0600

    Added hero image randomizer.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,4 @@
-
+$(function() {
+    var images = ['hero_face1.jpg', 'hero_face2.jpg', 'hero_face3.jpg'];
+    $('header').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
+});
