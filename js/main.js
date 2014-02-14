b95ab9059656166b9f6e033139198778d0baf77f
Coty Beasley <beacrea@users.noreply.github.com>
2014-02-14 00:53:59 -0600

    Added compass and animated and styled social media links.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-	$(function() {
+// Jumbotron Image Randomizer
+$(function() {
     var images = ['hero_face1.jpg', 'hero_face2.jpg'];
     $('header').css({'background-image': 'url(img/' + images[Math.floor(Math.random() * images.length)] + ')'});
 });
