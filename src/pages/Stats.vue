f406bb389c218563897e5ae32177e032b20dbf2b
Coty Beasley <coty@cotybeasley.name>
2018-03-03 18:48:46 -0800

    Added some computed bindings

--- src/pages/Stats.vue
+++ src/pages/Stats.vue
@@ -1,9 +1,40 @@
 <template>
   <main-layout>
     <div id="stats">
-      <li v-for="item in stats" :key="item.name">
-        {{ item.name }}
-      </li>
+      <section class="stats">
+        <header>
+          <h1>Current Stats</h1>
+          <span class="dice" ontouchstart="" alt="Roll the dice!"></span>
+        </header>
+        <div class="gaugeGroup">
+          <div class="stat" v-for="stat in stats" :key="stat.name">
+            <div class="label">
+              {{stat.name}}
+            </div>
+            <div class="guage">
+              <div class="fill">
+                {{statValue(stat.value)}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <h1>Equipment List</h1>
+        <div class="equipGroup">
+          <div class="item" v-for="item in items" :key="item.name">
+            <div class="itemBox">
+              A
+            </div>
+            <div class="content">
+              <header>{{item.name}}<span>({{item.type}})</span></header>
+              <div class="text">
+                {{item.desc}}
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   </main-layout>
 </template>
@@ -19,7 +50,13 @@
     data() {
       return {
         msg: 'This is the stats page.',
-        stats: model.items
+        stats: model.stats,
+        items: model.items
+      }
+    },
+    methods: {
+      statValue: function (value) {
+        return value + '%'
       }
     }
   }
@@ -31,6 +68,11 @@
 
   #stats {
     padding: $padding_side;
-    background: $color_blue-shade;
+    background: url('/static/img/stats-bg.jpg') no-repeat top left;
+    background-size: cover;
+    display: flex;
+    flex-direction: column;
+    overflow-y: scroll;
+    height: 100%;
   }
 </style>
