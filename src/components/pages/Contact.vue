f763a3142563c067e681d5d736adeb82836582fd
Coty Beasley <coty@cotybeasley.name>
2018-03-07 00:18:41 -0800

    Added contact links to contact page

--- src/components/pages/Contact.vue
+++ src/components/pages/Contact.vue
@@ -1,7 +1,27 @@
 <template>
   <div id="contact">
    <section>
-     <p>{{msg}}</p>
+     <h2>Where I'm Found</h2>
+     <div class="grid">
+        <div class="icon">
+          <a :href="icon.dribbble.link" target="_blank">
+            <img :src="setIcon(icon.dribbble.asset)" />
+          </a>
+          <span>{{icon.dribbble.title}}</span>
+        </div>
+        <div class="icon">
+          <a :href="icon.github.link" target="_blank">
+            <img :src="setIcon(icon.github.asset)" />
+          </a>
+          <span>{{icon.github.title}}</span>
+        </div>
+        <div class="icon">
+          <a :href="icon.linkedin.link" target="_blank">
+            <img :src="setIcon(icon.linkedin.asset)" />
+          </a>
+          <span>{{icon.linkedin.title}}</span>
+        </div>
+     </div>
    </section>
   </div>
 </template>
@@ -12,7 +32,40 @@ export default {
   components: {},
   data () {
     return {
-      msg: 'This is the contact page.'
+      msg: 'This is the contact page.',
+      iconPath: '../../../static/icons/contact/',
+      icon: {
+        dribbble: {
+          title: 'Dribbble',
+          link: 'http://dribbble.com/beacrea',
+          asset: 'dribbble.svg'
+        },
+        twitter: {
+          title: 'Twitter',
+          link: 'http://twitter.com/beacrea',
+          asset: 'twitter.svg'
+        },
+        github: {
+          title: 'Github',
+          link: 'http://github.com/beacrea',
+          asset: 'github.svg'
+        },
+        codepen: {
+          title: 'Codepen',
+          link: 'http://codepen.io/beacrea/',
+          asset: 'codepen.svg'
+        },
+        linkedin: {
+          title: 'Linkedin',
+          link: 'http://linkedin.com/in/cbeasley0',
+          asset: 'linkedin-in.svg'
+        }
+      }
+    }
+  },
+  methods: {
+    setIcon: function (i) {
+      return this.iconPath + i
     }
   }
 }
@@ -27,10 +80,31 @@ export default {
   display: flex;
   flex-direction: column;
   overflow: scroll;
-  height: 100%;
+  justify-content: center;
+  align-items: center;
   padding: $padding_side;
+  text-align: center;
   * {
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+}
+.grid {
+  display: grid;
+  text-align: center;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-column-gap: 1.6rem;
+}
+.icon {
+  img {
+    max-height: 4.0rem;
+    color: white;
+  }
+  a {
+    border-bottom: none;
+  }
+  & > span {
+    font-size: 1.4rem;
+    line-height: 1.5;
   }
 }
 </style>
