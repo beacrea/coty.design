994b1e6bf7f9b70d86eee4a375a1b83b4a5c95e3
Coty Beasley <beacrea@users.noreply.github.com>
2018-03-06 23:21:09 -0800

    Added contact page and background. Modified navigation.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import About from '../components/pages/About'
 import Stats from '../components/pages/Stats'
+import Contact from '../components/pages/Contact'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
       path: '/stats',
       name: 'stats',
       component: Stats
+    },
+    {
+      path: '/contact',
+      name: 'contact',
+      component: Contact
     }
   ]
 })
