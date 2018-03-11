7976cc012725113c1b78cb9480a3da3bf88dea6a
Coty Beasley <beacrea@users.noreply.github.com>
2018-03-10 19:50:08 -0800

    Added work page

---
+++ src/components/pages/Work.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="work">{{msg}}</div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+      msg: 'This is the work page.'
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// Imports
+@import "../../globalStyles/imports";
+@import "../../globalStyles/vars";
+
+#work {
+  display: flex;
+  flex-direction: column;
+  overflow: scroll;
+  justify-content: center;
+  align-items: center;
+  padding: $padding_side;
+  text-align: center;
+  * {
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+  }
+}
+</style>
