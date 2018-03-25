cc6560b2510ffea33f5c09e1a6639f44fe5c8aa6
Coty Beasley <beacrea@users.noreply.github.com>
2018-03-25 16:50:01 -0700

    Initial work on card grids

--- src/components/pages/Work.vue
+++ src/components/pages/Work.vue
@@ -5,15 +5,27 @@
       <p>Through the years I’ve worked on everything from bobbleheads, robotics, health outcomes, sports tech, chatbots, and more.</p>
       <p>A common thread in all my projects is a desire to solve complex problems with emerging technologies that have a chance to improve the world around me.</p>
     </header>
+    <section>
+      <div class="item" v-for="item in workItems" :key="item.id">
+        <div class="preview"></div>
+        <div class="content">
+          <h1>{{item.projectTitle}}</h1>
+          <div class="text">{{item.company.shortDescription}}</div>
+        </div>
+      </div>
+
+    </section>
   </div>
 </template>
 
 <script>
+import work from '../../../data/work'
+
 export default {
   components: {},
   data () {
     return {
-      msg: ''
+      workItems: work
     }
   }
 }
@@ -28,7 +40,6 @@ export default {
 #work {
   overflow: scroll;
   font-size: 1.6rem;
-  background-color: $color_dark;
   * {
     -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
     color: $color_light;
@@ -41,6 +52,46 @@ header {
   h1 {
     margin: 0;
     font-size: $h2_size;
+  }
+}
+section {
+  padding: 0 $padding_side*2 6.4rem;
+}
+.item {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+  margin: 6.4rem auto 0;
+  .preview, .content {
+    border-radius: 0.25rem;
+    box-shadow: 0.25rem 0.25rem 1rem rgba(black, 0.5);
+  }
+  .preview {
+    grid-column-start: 1;
+    grid-column-end: 12;
+    grid-row-start: 1;
+    grid-row-end: 7;
+    background: pink;
+  }
+  .content {
+    display: flex;
+    padding: 1.6rem;
+    grid-column-start: 2;
+    grid-column-end: 13;
+    grid-row-start: 5;
+    grid-row-end: 10;
+    background: red;
+  }
+}
+
+/* Tablets+ */
+@media screen and (min-width: 35rem) {
+  .item {
+    height: 400px;
+    margin-bottom: 3.2rem;
+  }
+  .content {
+    max-height: 15rem;
   }
 }
 </style>
