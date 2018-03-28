d3b5decb524189a82d258881cf52017f9e2117e2
Coty Beasley <coty@cotybeasley.name>
2018-03-27 22:47:04 -0700

    Created a simple express server that returns a json object on request

--- server/ping.js
+++ server/ping.js
@@ -1,6 +1,6 @@
 // Creds
-let accountSid = 'ACb4d5303cb66f41ea220f25ac6db15905'; // Your Account SID from www.twilio.com/console
-let authToken = 'c66b577b251c9ef18c7dbf7758b7dbbc';   // Your Auth Token from www.twilio.com/console
+let accountSid = null; // Your Account SID from www.twilio.com/console
+let authToken = null;   // Your Auth Token from www.twilio.com/console
 
 // Basics
 let twilio = require('twilio');
@@ -8,19 +8,14 @@ let client = new twilio(accountSid, authToken);
 
 // Phone Directory
 let contact = {
-  mindy: '+19134268396',
-  coty: '+18162002689',
-  beasleyBot: '+14158401420',
-  sean: '+18162001161',
-  liz: '+18282283437',
-  pitichoke: '+12064651048',
-  ariel: '+12103265459'
+  jenny: '+18675309',
+  bruce: '+17762323'
 };
 
 // Data Vars
 let currentTime = new Date();
-let toNumber = contact.ariel;
-let fromNumber = contact.beasleyBot;
+let toNumber = contact.jenny;
+let fromNumber = contact.bruce;
 
 
 // Message Magic
