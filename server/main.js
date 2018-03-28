a9de7db9c2e7a9da06926c41aac812797bd3a0ad
Coty Beasley <coty@cotybeasley.name>
2018-03-27 22:49:22 -0700

    Changed express port

--- server/main.js
+++ server/main.js
@@ -5,4 +5,4 @@ app.set('json spaces', 40)
 
 app.get('/', (req, res) => res.json({ coolguy: 'Dat boi, C-Beez' }))
 
-app.listen(9000, () => console.log('Example app listening on port 9000!'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
